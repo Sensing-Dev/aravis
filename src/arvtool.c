55626c3dff2f706f3d5879ca3c953840adde0456
Momoko Kono <momoko.kono@fixstars.com>
2023-05-12 08:56:42 +0900

    got rid of LOG

--- src/arvtool.c
+++ src/arvtool.c
@@ -270,7 +270,6 @@ static void
 arv_tool_control (int argc, char **argv, ArvDevice *device)
 {
         int i;
-        printf("[LOG] arv_tool_control::\n");
 
         for (i = 2; i < argc; i++) {
                 ArvGcNode *feature;
@@ -372,26 +371,17 @@ arv_tool_control (int argc, char **argv, ArvDevice *device)
                                                 if (error == NULL)
                                                         printf ("%s = %s\n", tokens[0], value ?  "true" : "false");
                                         } else if (ARV_IS_GC_REGISTER (feature)) {
-                                                printf("[LOG] arv_tool_control::The type of this node is Register\n");
-                                                char buffer[2048];
+                                                char* buffer;
 
                                                 guint64 length = arv_gc_register_get_length(ARV_GC_REGISTER (feature), &error);
-                                                printf("[LOG] arv_tool_control::Buffer length is %" G_GUINT64_FORMAT "\n", length);
+                                                if (error == NULL)
+                                                        printf ("Length of %s = %"G_GUINT64_FORMAT"\n", tokens[0], length);
 
+                                                buffer = (char*) malloc(length);
                                                 arv_gc_register_get (ARV_GC_REGISTER (feature), (void*)buffer, length, &error);
 
-                                                if (buffer == NULL){
-                                                        printf("[LOG] arv_tool_control::Buffer is empty\n");
-                                                }
-                                                char signature[4];
-                                                memcpy(signature, buffer, 4);
-
-                                                printf("[LOG] arv_tool_control:: the first 4 byte of gendc descriptor = [%s](string)\n", &signature);
-                                                printf("[LOG] arv_tool_control:: the first 4 byte of gendc descriptor = [%d](int)\n", *((int*)signature));
-                                                printf("[LOG] arv_tool_control:: the whole %" G_GUINT64_FORMAT " byte of gendc descriptor = [%d](int)\n", length, *(int*)buffer);
-
-                                                if (buffer != NULL){
-                                                        printf("[LOG] arv_tool_control::buffer if not NULL:");
+                                                if (error == NULL){
+                                                        printf("Content of %s =", tokens[0]);
                                                         for( int i = 0; i < length; i++){
                                                                 if ( i%8 == 0){
                                                                         printf("\n\t");
@@ -399,8 +389,7 @@ arv_tool_control (int argc, char **argv, ArvDevice *device)
                                                                 printf("0x%02x ", *(((char*)buffer)+i));
                                                         }
                                                 }
-
-
+                                                free(buffer);
                                         }else {
                                                 const char *value =  arv_gc_feature_node_get_value_as_string
                                                         (ARV_GC_FEATURE_NODE (feature), &error);
